8aca86ff695f315339dddcf929bb5ef5c5cde33c
MuhammadAmmarDigitalWaze <amaar.digitalwaze@gmail.com>
2020-12-20 02:06:40 +0500

    stablev1

--- src/authPages/signup/signup.jsx
+++ src/authPages/signup/signup.jsx
@@ -1,25 +1,27 @@
-import React, { Component } from 'react';
-import { Paper,Grid } from '@material-ui/core';
+import React, {Component} from 'react';
+import {Paper, Grid} from '@material-ui/core';
 
 class Signup extends Component {
     constructor(props) {
         super(props);
-        this.state = {  }
+        this.state = {}
     }
-    render() { 
+    render() {
 
-        const classes = { paper:{ background:'yellow' } }
+        const classes = {
+            paper: {
+                background: 'yellow'
+            }
+        }
 
-        return ( 
-            <Grid container >
+        return (
+            <Grid container>
                 <Grid item xs={12} sm={12} md={8} lg={8} xl={6}>
-                    <Paper className={classes.paper}>
-
-                    </Paper>
+                    <Paper className={classes.paper}></Paper>
                 </Grid>
-            </Grid> 
+            </Grid>
         );
     }
 }
- 
+
 export default Signup;
